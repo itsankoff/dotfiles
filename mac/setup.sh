56e854c7dcb69774f94ca45468fbeef29296f9a8
itsankoff <itsankoff@gmail.com>
2018-07-24 12:18:54 +0300

    * Add more tools in mac setup

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install git
+brew install git
+
 # Setup development structure
 DEV_HOME=~developers
 mkdir $DEV_HOME
@@ -10,6 +13,13 @@ mkdir -p $DEV_HOME/workspace/src
 mkdir -p $DEV_HOME/workspace/pkg
 mkdir -p $DEV_HOME/workspace/bin
 
+# install golang
+brew install go
+brew install hg
+
+# Install goimports
+go get golang.org/x/tools/cmd/goimports
+
 # Install basic apps
 brew cask install iterm2
 brew cask install google-chrome
@@ -19,10 +29,8 @@ brew install htop
 brew install git
 brew install direnv
 brew install autojump
-brew install go
-brew install hg
 
-# Postgres
+# Install postgres
 brew install postgresql
 initdb
 gem install lunchy
@@ -43,13 +51,9 @@ ln -s ~/developers/dotfiles/.aliases ~/.aliases
 # Install zsh themes
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-# Enable zsh config
-source ~/.zshrc
-
 # Install golang
 wget https://dl.google.com/go/go1.10.3.darwin-amd64.pkg
 tar -C /usr/local -xzf /usr/local go1.10.3.darwin-amd64.pkg
-
 
 # Clone dotfiles repo
 pushd ~/developers && git clone git@github.com:itsankoff/dotfiles.git popd
@@ -59,3 +63,8 @@ pushd ~/developers/dotfiles/vim && ./install.sh && popd
 
 # Setup github
 ln -s ~/developers/dotfiles/.gitconfig ~/.gitconfig
+
+# Reload zsh config
+source ~/.zshrc
+
+echo Done!
