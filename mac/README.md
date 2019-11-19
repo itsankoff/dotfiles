f9f98c4424a3f22b57045e31719d86e209b9a15b
Ivaylo Tsankov <itsankoff@gmail.com>
2019-11-19 10:52:48 -0800

    * Fix some quirks

---
+++ mac/README.md
@@ -0,0 +1,4 @@
+# Installation guide for OSX
+
+## Install AirPodsConnect
+* [Read here](https://medium.com/@secondfret/how-to-connect-your-airpods-to-your-mac-with-a-keyboard-shortcut-9d72e786993b)
--- mac/setup.sh
+++ mac/setup.sh
@@ -3,6 +3,10 @@
 # NOTE:
 # This file can work as stanalone installer for mac environment
 
+function message {
+    echo "$1"
+}
+
 # Setup machine hostname
 sudo scutil --set HostName molar
 
@@ -10,33 +14,32 @@ sudo scutil --set HostName molar
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install basic apps
-brew install git
-brew install terraform
-brew install wget
-brew install curl
-brew install htop
-brew install git
-brew install direnv
-brew install autojump
-brew install grip
-brew install node
-brew install ansible
-brew install python3
-brew install vim
-brew install cmake
-brew install ffmpeg
-pip3 install virtualenv
-brew cask install iterm2
-brew cask install the-unarchiver
-brew cask install google-chrome
-brew cask install java
-brew cask install docker
-brew cask install tunnelblick
-brew cask install ngrok
-brew cask install skype
-brew cask install slack
-brew cask install spotify
-sudo gem install cocoapods
+brew install git || message "Failed to install git"
+brew install terraform || message "Failed to install terraform"
+brew install wget || message "Failed to install wget"
+brew install curl || message "Failed to install curl"
+brew install htop || message "Failed to install htop"
+brew install direnv || message "Failed to install direnv"
+brew install autojump || message "Failed to install autojump"
+brew install grip || message "Failed to install grip"
+brew install node || message "Failed to install node"
+brew install ansible || message "Failed to install ansible"
+brew install python3 || message "Failed to install python3"
+brew install vim || message "Failed to install vim"
+brew install cmake || message "Failed to install cmake"
+brew install ffmpeg || message "Failed to install ffmpeg"
+pip3 install virtualenv || message "Failed to install virtualenv"
+brew cask install iterm2 || message "Failed to install iterm2"
+brew cask install the-unarchiver || message "Failed to install the-unarchiver"
+brew cask install google-chrome || message "Failed to install Chrome"
+brew cask install java || message "Failed to install java"
+brew cask install docker || message "Failed to install docker"
+brew cask install tunnelblick || message "Failed to install tunnelblick"
+brew cask install ngrok || message "Failed to install ngrok"
+brew cask install skype || message "Failed to install skype"
+brew cask install slack || message "Failed to install slack"
+brew cask install spotify || message "Failed to install spotify"
+sudo gem install cocoapods || message "Failed to install cocoapods"
 
 # Setup development structure
 USER_HOME="/Users/${USER}"
@@ -59,6 +62,7 @@ go get golang.org/x/tools/cmd/goimports
 
 # Install postgres
 brew install postgresql
+sleep 10
 initdb
 brew services start postgres
 createdb $USER
