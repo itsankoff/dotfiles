f086b0f1b6b199b30e1488ae60a5f8cc4f627fc2
Ivaylo Tsankov <itsankoff@gmail.com>
2021-02-09 20:18:37 +0200

    * Extrack 'all' packages in separate variable

--- mac/lib.sh
+++ mac/lib.sh
@@ -246,7 +246,8 @@ function pkg_install() {
     fi
 }
 
-PACKAGE="all"
+ALL_PACKAGES="all"
+PACKAGE=${ALL_PACKAGES}
 # is_pkg is used for conditional package installation. If the ${1} is the name
 # of the package provided by the CLI args then return 0 (true)
 # otherwise 1 (false).
@@ -256,7 +257,7 @@ PACKAGE="all"
 # NOTE: In case no package is provided by CLI or as environment variable then
 # "all" is assumed which will allow all package installations and configuration.
 function is_pkg() {
-    if [[ ${PACKAGE} == "all" ]] || [[ ${PACKAGE} == ${1} ]]
+    if [[ ${PACKAGE} == ${ALL_PACKAGES} ]] || [[ ${PACKAGE} == ${1} ]]
     then
         return 0
     else
